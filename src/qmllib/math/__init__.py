3c5c41b40063ea7dbada247e2fe1f491bb7348c9
Anders S. Christensen <andersbiceps@gmail.com>
2017-04-25 14:43:07 +0200

    Added a lot of stuff

---
+++ src/qmllib/math/__init__.py
@@ -0,0 +1,33 @@
+
+#
+
+#
+
+
+
+
+
+
+#
+
+
+#
+
+
+
+
+
+
+
+
+"""
+QML math routines
+=====
+Provides
+  1. cho_solve -- Cholesky solver
+  2. cho_invert -- Cholesky inversion
+"""
+
+from .cho_solve import cho_solve, cho_invert
+
+__all__ = ['cho_solve', "cho_invert"]
---
+++ src/qmllib/representations/__init__.py
@@ -0,0 +1,38 @@
+
+#
+
+#
+
+
+
+
+
+
+#
+
+
+#
+
+
+
+
+
+
+
+
+"""
+FML representation
+=====
+Provides
+  1. coulomb_matrix
+  1. local_coulomb_matrix
+"""
+
+from frepresentations import fgenerate_coulomb_matrix
+from frepresentations import fgenerate_unsorted_coulomb_matrix
+from frepresentations import fgenerate_local_coulomb_matrix
+from frepresentations import fgenerate_atomic_coulomb_matrix
+from arad import ARAD
+
+__all__ = ['fgenerate_coulomb_matrix']
+
