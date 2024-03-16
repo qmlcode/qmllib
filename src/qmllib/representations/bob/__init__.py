030824ee18eed11930ae98f798111fb146d4f899
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-03-16 18:16:17 +0100

    Added utils functions for BoB

---
+++ src/qmllib/representations/bob/__init__.py
@@ -0,0 +1,42 @@
+"""
+Bag of bonds utils functions
+"""
+
+import numpy as np
+
+from qmllib.constants.periodic_table import ELEMENT_NAME
+
+
+def get_natypes(nuclear_charges: np.ndarray) -> dict[str, int]:
+    """Get number of atom types, from a list of molecules"""
+
+    keys, counts = np.unique(nuclear_charges, return_counts=True)
+
+    # natypes = dict([(key, len(value)) for key,value in self.atomtype_indices.items()])
+
+    # natypes = dict([ (key, count) for key, count in zip(keys, counts)])
+
+    keys_name = [ELEMENT_NAME[key] for key in keys]
+
+    natypes = dict([(key, count) for key, count in zip(keys_name, counts)])
+
+    return natypes
+
+
+def get_asize(list_nuclear_charges, pad) -> dict[str, int]:
+    """
+
+    example:
+        asize = {"O":3, "C":7, "N":3, "H":16, "S":1}
+    """
+
+    asize: dict[str, int] = dict()
+
+    for nuclear_charges in list_nuclear_charges:
+        natypes = get_natypes(nuclear_charges)
+        for key, value in natypes.items():
+            try:
+                asize[key] = max(asize[key], value + pad)
+            except KeyError:
+                asize[key] = value + pad
+    return asize
