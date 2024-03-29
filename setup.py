3ec50df052bb84b6e0370b55e56cffc55f057ee4
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-03-29 21:13:40 +0100

    Duplicate information for pypi

--- setup.py
+++ setup.py
@@ -11,4 +11,16 @@ except ImportError:
 
 if __name__ == "__main__":
     _compile.main()
-    setup()
+    setup(
+        description="Python/Fortran toolkit for representation of molecules and solids for machine learning of properties of molecules and solids.",
+        classifiers=[
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "Topic :: Scientific/Engineering :: Chemistry",
+        ],
+        keywords=["qml", "quantum chemistry", "machine learning"],
+    )
